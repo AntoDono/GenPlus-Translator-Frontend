04dc8c2ed4e2af58baee3b52874941bb759a8a8f
AntoDono <anthony20151128@gmail.com>
2024-09-19 04:01:37 -0400

    initial

---
+++ pages/index.tsx
@@ -0,0 +1,9 @@
+import '../app/globals.css'
+
+export default function index() {
+  return (
+    <div className="w-screen h-[80vh] text-5xl flex justify-center items-center font-[SUSE] font-black">
+      <h1>Gen+ Translator</h1>
+    </div>
+  );
+}
